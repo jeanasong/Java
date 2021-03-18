e978be41f1de6876ccda218a0c786bbe1486702f
jeanasong <jeanasong46@yahoo.com>
2021-03-18 16:42:59 -0400

    Update with comments

--- Mod3_WK1_HW1/src/main/Parrot.java
+++ Mod3_WK1_HW1/src/main/Parrot.java
@@ -1,5 +1,18 @@
 package main;
 
+
+// Instructions below
+
+//We have a parrot that likes to squawk at night in its cage when it’s scared. Write a method named getUp that has 2 parameters
+//
+//1st parameter  should be a boolean and be named squawking it represent the parrot.
+//
+//2nd parameter represents the hour of the day and is an int named currentHour has a valid range of 0-23.
+//
+//We have to get up if the parrot is squawking before 6 or after 22 hours so in that case return true.
+//
+//All other cases will return false. If the currentHour parameter is less than 0 or greater than 23 return false
+
 public class Parrot {
 
 	public static void main(String[] args) {
